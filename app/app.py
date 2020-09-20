65aedc1a6a0c16dd87d96266aeeddd1173b52808
jacekfilipczuk <jacekfilipczuk89@gmail.com>
2020-09-20 13:52:43 +0100

    deleted all functions

--- app/app.py
+++ app/app.py
@@ -1,12 +1,6 @@
 import dash
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-from datetime import datetime as dt
 from flask_caching import Cache
-import pandas as pd
-import uuid
-import re
+
 
 external_stylesheets = [
     # Dash CSS
@@ -28,22 +22,6 @@ cache = Cache(app.server, config={
     'CACHE_THRESHOLD': 200
 })
 
-
-def get_dataframe(session_id):
-    @cache.memoize()
-    def query_and_serialize_data(session_id):
-        # expensive or user/session-unique data processing step goes here
-
-        # simulate a user/session-unique data processing step by generating
-        # data that is dependent on time
-
-        # simulate an expensive data processing task by sleeping
-        # time.sleep(5)
-        df = pd.read_csv('../data/published_questions_data_2018-2020_KEK.csv')
-        df.rename(columns={'platform_name': 'clients'}, inplace=True)
-        return df.to_json()
-
-    return pd.read_json(query_and_serialize_data(session_id))
 
 
 
