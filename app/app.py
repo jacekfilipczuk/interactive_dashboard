38419ce6604d106d7189a5c34308326eb74e90ed
jacekfilipczuk <jacekfilipczuk89@gmail.com>
2020-09-21 16:43:24 +0100

    added import for layout skeleton

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 import dash
 from config import external_stylesheets, app_name, meta_tags_arg
+from layouts.layout_skeleton import create_layout_skeleton
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[meta_tags_arg])
 app.title = app_name
 app.config.suppress_callback_exceptions = True
-app.layout = serve_layout
+app.layout = create_layout_skeleton()
 
 
 if __name__ == '__main__':
